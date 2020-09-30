7227f4e3976950837419a84f8a2231797cc99475
Khalak97 <valodyakh@gmail.com>
2020-09-30 10:35:25 +0300

    header

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAWYl3W35W10yi3OwfOMb4H6T86IY9gZnM",
+  authDomain: "react-facebook-aaf57.firebaseapp.com",
+  databaseURL: "https://react-facebook-aaf57.firebaseio.com",
+  projectId: "react-facebook-aaf57",
+  storageBucket: "react-facebook-aaf57.appspot.com",
+  messagingSenderId: "953197557733",
+  appId: "1:953197557733:web:a20fb60e41eec43732610c",
+};
